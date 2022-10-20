963ba2f2e7936184ce6bf05d6cc33e6a4bde1ecc
Juliana Guonik <juliana_guonik@hotmail.com>
2022-10-20 14:53:53 -0300

    chess layer and printing the board - fix bug

--- src/application/UI.java
+++ src/application/UI.java
@@ -9,17 +9,15 @@ public class UI {
 			System.out.print((8 - i) + " ");
 			for (int j=0; j<pieces.length; j++) {
 				printPiece(pieces[i][j]);
-				
 			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
-		
 	}
-	
+
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
-			System.out.println("-");
+			System.out.print("-");
 		}
 		else {
 			System.out.print(piece);
